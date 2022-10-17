e1f82b7e7b679581dd342a1dc174fb5f1e47c51a
jenya <jenek888q@gmail.com>
2022-10-17 03:24:14 +0300

    fix

--- Backend/Shop/Startup.cs
+++ Backend/Shop/Startup.cs
@@ -77,9 +77,9 @@ namespace Shop
         private void AddDbContext(IServiceCollection services)
         {
             var locaConnection = Configuration.GetConnectionString("DefaultConnection");
-            var serverconnection = "workstation id=ShopyDb.mssql.somee.com;packet size=4096;user id=evgklns_SQLLogin_1;pwd=3p8nqpu79w;data source=ShopyDb.mssql.somee.com;persist security info=False;initial catalog=ShopyDb ";
+            
             services.AddDbContext<ShopDbContext>(options =>
-            options.UseSqlServer(serverconnection)
+            options.UseSqlServer(locaConnection)
            // options.UseSqlite($"Filename=D:\\ShopDb.db")
             .UseLazyLoadingProxies());
         }
